2662337c5b16292c7c6970a043f548427d173ace
Gabiru <gabriel.cerqueiraa05@gmail.com>
2025-07-09 19:31:34 -0300

    Adição de função Luta

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 import 'dart:math';
 
@@ -20,47 +21,73 @@ void main() {
 
   String item = stdin.readLineSync()!;
   if (item == "1") {
-    format(formatacao: "$item1");
+    formatarTexto(formatacao: "$item1");
   } else if (item == "2") {
-    format(formatacao: "$item2");
+    formatarTexto(formatacao: "$item2");
   } else if (item == "3") {
-    format(formatacao: "$item3");
+    formatarTexto(formatacao: "$item3");
   } else {
     print("Escolha um número de 1 a 3");
   }
 
-  //Agora Irá começar a luta contra o lobisomem Pidão
+  //Agora Irá começar a luta contra o Monstro
+
   print(
       "Agora que você escolheu a sua arma, você irá lutar contra o $nomeMonstro");
   print("A LUTA SERA : $usuario X $nomeMonstro");
   print("$nomeMonstro --- Me vê uma pouquinho de cuscuz kkkk");
   luta(nomeMonstro: "Bisteca Mágica", vidaa: 100);
-  
 }
 
-void format({String? formatacao}) {
+void formatarTexto({String? formatacao}) {
   print("O item escolhido é $formatacao");
 }
 
-void luta({String? nomeMonstro, int? vidaa}) {
+int? lutarMonstro({required String nomeMonstro, required int vida}) {
+  print("VOCÊ IRÁ JOGAR O DADO PARA VER SE IRÁ GANHAR DO $nomeMonstro");
+  var d10 = Random().nextInt(10);
+  print("O valor do dado de 1 a 10 foi de $d10");
+  if (d10 < 5) {
+    vida = vida - 10;
+    print("sua vida esta com o saldo de $vida");
+    return vida;
+  } else {
+    print("Você ganhou do $nomeMonstro");
+    return -1;
+  }
+}
+
+void luta({required nomeMonstro, required int vidaa}) {
   int? vida = vidaa;
   while (vida! > 0) {
-    print("Escreva 1 para fugir do $nomeMonstro , ou 2 para lutar com o $nomeMonstro ");
+    print(
+        "Escreva 1 para fugir do $nomeMonstro , ou 2 para lutar com o $nomeMonstro ");
     var lutar = stdin.readLineSync();
     if (lutar == "1") {
-      print("Você fugiu do $nomeMonstro");
-      break;
-    } else if (lutar == "2") {
-      print("VOCÊ IRÁ JOGAR O DADO PARA VER SE IRÁ GANHAR DO $nomeMonstro");
-      var d10 = Random().nextInt(10);
-      print("O valor do dado de 1 a 10 foi de $d10");
-      if (d10 < 5) {
-        vida = vida - 10;
-        print("sua vida esta com o saldo de $vida");
-      } else if (d10 > 5) {
-        print("Você ganhou do $nomeMonstro");
+      var d10Luta = Random().nextInt(10);
+      print(
+          "Você irá ser capaz de fugir de acordo com o valor do dado, o valor do dado é $d10Luta");
+      if (d10Luta > 5) {
+        print("Você conseguiu fugir do Monstro");
         break;
+      } else {
+        var resultado2 = lutarMonstro(nomeMonstro: nomeMonstro, vida: vida);
+        if (resultado2 == -1) {
+          break;
+        } else {
+          vida = resultado2;
+        }
       }
+    } else if (lutar == "2") {
+      var resultado = lutarMonstro(nomeMonstro: nomeMonstro, vida: vida);
+      if (resultado == -1) {
+        break;
+      } else {
+        vida = resultado;
+      }
+    } else {
+      print("Digite os Números 1 ou 2 ");
     }
+    ;
   }
 }
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,3 @@
+import 'dart:math';
+void main(){var d10Luta = Random().nextInt(2);
+print(d10Luta);}
